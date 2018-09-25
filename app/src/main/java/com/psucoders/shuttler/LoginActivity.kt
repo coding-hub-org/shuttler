f5c1ffc500c3163b98189bc80a9dfcdaea515031
gonzen2310 <gonzaloreyesllauca@gmail.com>
2018-09-25 01:38:26 -0500

    Hooked button and intent

--- app/src/main/java/com/psucoders/shuttler/LoginActivity.kt
+++ app/src/main/java/com/psucoders/shuttler/LoginActivity.kt
@@ -1,9 +1,9 @@
 package com.psucoders.shuttler
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.View.OnTouchListener
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_login.*
 
 
@@ -13,5 +13,13 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        login(btnSignIn)
+    }
+
+    fun login(button: Button) {
+        button.setOnClickListener {
+            val intent = Intent(this@LoginActivity, TrackerActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
