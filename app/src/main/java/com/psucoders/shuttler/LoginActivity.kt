35a818de2d71b7bb60bc568f8224ce21ee5ed815
gonzen2310 <gonzaloreyesllauca@gmail.com>
2018-09-25 01:21:29 -0500

    Added password toggler

--- app/src/main/java/com/psucoders/shuttler/LoginActivity.kt
+++ app/src/main/java/com/psucoders/shuttler/LoginActivity.kt
@@ -2,11 +2,16 @@ package com.psucoders.shuttler
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View.OnTouchListener
+import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
     }
 }
