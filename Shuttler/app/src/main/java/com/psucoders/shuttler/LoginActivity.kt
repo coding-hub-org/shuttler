9dc992ddfdcfe401f6f5d498579289638cca48a9
gonzen2310 <gonzaloreyesllauca@gmail.com>
2018-09-26 19:50:07 -0500

    Linked login and maps activity

--- Shuttler/app/src/main/java/com/psucoders/shuttler/LoginActivity.kt
+++ Shuttler/app/src/main/java/com/psucoders/shuttler/LoginActivity.kt
@@ -1,12 +1,24 @@
 package com.psucoders.shuttler
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        signIn(btnSignIn)
+    }
+
+    fun signIn(button: Button) {
+        button.setOnClickListener {
+            val intent = Intent(this, TrackerActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
