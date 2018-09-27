d29afb952e7a49a5f2ecfceae2feffbd94382b23
gonzen2310 <gonzaloreyesllauca@gmail.com>
2018-09-27 02:17:09 -0500

    Firebase authentication working

--- Shuttler/app/src/main/java/com/psucoders/shuttler/LoginActivity.kt
+++ Shuttler/app/src/main/java/com/psucoders/shuttler/LoginActivity.kt
@@ -3,22 +3,49 @@ package com.psucoders.shuttler
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
+import com.google.firebase.auth.FirebaseAuth
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        signIn(btnSignIn)
+        mAuth = FirebaseAuth.getInstance()
+
+        btnSignIn.setOnClickListener {
+            val email = edtUser.text.toString()
+            val password = edtPassword.text.toString()
+            signIn(email, password)
+        }
     }
 
-    fun signIn(button: Button) {
-        button.setOnClickListener {
+    fun signIn(email: String, password: String) {
+
+        // FIRE BASE AUTH TO CREATE NEW USER
+        mAuth!!.createUserWithEmailAndPassword(email, password).addOnCompleteListener{ it ->
+            if (!it.isSuccessful) {
+                Toast.makeText(this@LoginActivity, "FAIL TO CREATE ACCOUNT", Toast.LENGTH_SHORT).show()
+                return@addOnCompleteListener
+            }
+
+            Log.d("FIREBASE REGISTRATION", "success to create user UID: ${it.result.user.uid}")
+            var user = mAuth!!.currentUser
+            user!!.sendEmailVerification().addOnCompleteListener {
+                Toast.makeText(this@LoginActivity, "Verify Email", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
+        /*button.setOnClickListener {
             val intent = Intent(this, TrackerActivity::class.java)
             startActivity(intent)
-        }
+        }*/
     }
 }
