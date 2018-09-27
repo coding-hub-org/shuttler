4893b508e8660075daa3672c30785ff6525b0fdc
gonzen2310 <gonzaloreyesllauca@gmail.com>
2018-09-27 13:12:35 -0500

    Fix logging authentication

--- Shuttler/app/src/main/java/com/psucoders/shuttler/LoginActivity.kt
+++ Shuttler/app/src/main/java/com/psucoders/shuttler/LoginActivity.kt
@@ -11,13 +11,16 @@ import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
-    private var mAuth: FirebaseAuth? = null
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         mAuth = FirebaseAuth.getInstance()
+
+        // Sing out user
+        mAuth.signOut()
 
         btnSignIn.setOnClickListener {
             val email = edtUser.text.toString()
@@ -27,25 +30,23 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun signIn(email: String, password: String) {
+//        val intent = Intent(this, TrackerActivity::class.java)
+//        startActivity(intent)
 
-        // FIRE BASE AUTH TO CREATE NEW USER
-        mAuth!!.createUserWithEmailAndPassword(email, password).addOnCompleteListener{ it ->
-            if (!it.isSuccessful) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { it ->
+            if (it.isSuccessful) {
+                Log.d("FIREBASE REGISTRATION", "success to create user UID: ${it.result.user.uid}")
+                val user = mAuth.currentUser
+                user!!.sendEmailVerification().addOnCompleteListener {
+                    Toast.makeText(this@LoginActivity, "Verify Email", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            else {
                 Toast.makeText(this@LoginActivity, "FAIL TO CREATE ACCOUNT", Toast.LENGTH_SHORT).show()
                 return@addOnCompleteListener
             }
-
-            Log.d("FIREBASE REGISTRATION", "success to create user UID: ${it.result.user.uid}")
-            var user = mAuth!!.currentUser
-            user!!.sendEmailVerification().addOnCompleteListener {
-                Toast.makeText(this@LoginActivity, "Verify Email", Toast.LENGTH_SHORT).show()
-            }
-
-
         }
-        /*button.setOnClickListener {
-            val intent = Intent(this, TrackerActivity::class.java)
-            startActivity(intent)
-        }*/
+
     }
 }
