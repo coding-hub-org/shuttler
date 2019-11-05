93c965971f791b95df590d98505b082ad3e851c2
gonzen2310 <gonzaloreyesllauca@gmail.com>
2019-11-05 10:54:59 -0500

    Set up dynamic link in manifest

--- app/src/main/java/com/psucoders/shuttler/ui/login/LoginActivity.kt
+++ app/src/main/java/com/psucoders/shuttler/ui/login/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.psucoders.shuttler.ui.login
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,6 +16,7 @@ import kotlinx.android.synthetic.main.login_activity.*
 import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.auth.ActionCodeSettings
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks
 import com.psucoders.shuttler.ui.driver.DriverActivity
 import com.psucoders.shuttler.ui.forgotPassword.ForgotPassword
 
@@ -38,7 +41,27 @@ class LoginActivity : AppCompatActivity() {
                         "21" /* minimumVersion */)
                 .build()
 
-        val auth = FirebaseAuth.getInstance()
+//        val auth = FirebaseAuth.getInstance()
+
+        FirebaseDynamicLinks.getInstance()
+                .getDynamicLink(intent)
+                .addOnSuccessListener(this) { pendingDynamicLinkData ->
+                    // Get deep link from result (may be null if no link is found)
+                    var deepLink: Uri? = null
+                    if (pendingDynamicLinkData != null) {
+                        deepLink = pendingDynamicLinkData.link
+                        Toast.makeText(this, "Found deep link!", Toast.LENGTH_LONG).show()
+                    }
+
+//                    // Display deep link in the UI
+//                    if (deepLink != null) {
+//                        Toast.makeText(this, "Found deep link!", Toast.LENGTH_LONG).show()
+//                    } else {
+//                        Toast.makeText(this, "Not Found deep link!", Toast.LENGTH_LONG).show()
+//                        Log.d("LoginActivity", "getDynamicLink: no link found")
+//                    }
+                }
+                .addOnFailureListener(this) { e -> Log.w("LoginActivity", "getDynamicLink:onFailure", e) }
 
         loginFAB.setOnClickListener {
             Toast.makeText(this, "Click", Toast.LENGTH_LONG).show()
